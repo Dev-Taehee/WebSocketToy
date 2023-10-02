be9d087852fc813e292a1385c6ce2457d4f74229
TaeheeWoo94 <wth0086@gmail.com>
2023-10-02 16:01:03 +0900

    WebSocketAPIToy Start

---
+++ WebSocketAPIToy/src/main/java/com/example/WebSocketAPIToy/WebSocketApiToyApplication.java
@@ -0,0 +1,13 @@
+package com.example.WebSocketAPIToy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketApiToyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketApiToyApplication.class, args);
+	}
+
+}
---
+++ WebSocketAPIToy/src/test/java/com/example/WebSocketAPIToy/WebSocketApiToyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebSocketAPIToy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketApiToyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
